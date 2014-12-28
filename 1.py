d4e163edbf8afc5bb10c28df8a2b7eccec704a3a
Zhao <LittleZhao@users.noreply.github.com>
2014-12-28 20:08:52 +0800

    first level

---
+++ 1.py
@@ -0,0 +1,13 @@
+#http://www.pythonchallenge.com/pc/def/map.html
+import string
+s = "g fmnc wms bgblr rpylqjyrc gr zw fylb. rfyrq ufyr amknsrcpq ypc dmp. bmgle gr gl zw fylb gq glcddgagclr ylb rfyr'q ufw rfgq rcvr gq qm jmle. sqgle qrpgle.kyicrpylq() gq pcamkkclbcb. lmu ynnjw ml rfc spj."
+result = []
+map = string.maketrans('abcdefghijklmnopqrstuvwxyz', 'cdefghijklmnopqrstuvwxyzab')
+print s.translate(map)
+'''
+for i in s:
+    if ord(i) in range(97, 123):
+        print chr((ord(i) + 2 - 97) % 26 + 97),
+    else:
+        print i,
+'''
